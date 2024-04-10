9bd35704ca09a018e6da06abbba8bdf238b43f3b
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-10 18:37:19 +1200

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,3 @@
-Combo_meals = {
-    "Value": {
-        "Beef Burger": 5.69,
-        "Fries": 1.00,
-        "Fizzy Drink": 1.00
-    },
-    "Cheezy": {
-        "Cheeseburger": 6.69,
-        "Fries": 1.00,
-        "Fizzy Drink": 1.00
-    },
-    "Super": {
-        "Cheeseburger": 6.69,
-        "Large Fries": 2.00,
-        "Smoothie": 2.00
-    }
-}
-
-for combo_name, items_prices in Combo_meals.items():
-    print(f"\nCombo Name: {combo_name}\nItems & Prices:")
-
-    for key in items_prices:
-        print(f"{key}: {items_prices[key]}")
-
----------------------------------------------------------------------------
-
 import easygui
 
 # Get the input as a string
