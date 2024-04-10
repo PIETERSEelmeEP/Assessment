56aefc56c4860add78bbdc829b5318c1e4473423
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-10 18:00:39 +1200

    Update Information_Welcome_Message_v2.py

--- Information_Welcome_Message_v2.py
+++ Information_Welcome_Message_v2.py
@@ -3,8 +3,12 @@
 import easygui
 
 
+# Welcome & Selected Action Function
 def welcome_selected_action():
+    # Welcome Message
     easygui.msgbox("Welcome", title="Welcome Message")
+
+    # Select Action for Combo Menu Message & Buttonbox
     easygui.buttonbox("Please select action for the Combo Menu:",
                       title="Select Action", choices=["Add", "Search",
                                                       "Delete",
@@ -12,4 +16,5 @@ def welcome_selected_action():
                                                       "Exit Program"])
 
 
+# Main Routine
 welcome_selected_action()
