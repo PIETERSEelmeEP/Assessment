18179b6dfbe0dc5bd2b43525ac6143ce8c643785
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-10 18:35:04 +1200

    Create Store_Original_Combo_Meals_v1.py

---
+++ Store_Original_Combo_Meals_v1.py
@@ -0,0 +1,27 @@
+"""Create dictionaries within nested dictionaries. These are the original combo
+menu, consisting of items with each item's price
+"""
+
+Combo_meals = {
+    "Value": {
+        "Beef Burger": 5.69,
+        "Fries": 1.00,
+        "Fizzy Drink": 1.00
+    },
+    "Cheezy": {
+        "Cheeseburger": 6.69,
+        "Fries": 1.00,
+        "Fizzy Drink": 1.00
+    },
+    "Super": {
+        "Cheeseburger": 6.69,
+        "Large Fries": 2.00,
+        "Smoothie": 2.00
+    }
+}
+
+for combo_name, items_prices in Combo_meals.items():
+    print(f"\nCombo Name: {combo_name}\nItems & Prices:")
+
+    for key in items_prices:
+        print(f"{key}: {items_prices[key]}")
