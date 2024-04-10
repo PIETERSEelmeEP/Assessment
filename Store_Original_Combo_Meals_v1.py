3e57cbb095ef62fb1d45f1f48b987f83e622c622
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-10 18:53:55 +1200

    Update Store_Original_Combo_Meals_v1.py

--- Store_Original_Combo_Meals_v1.py
+++ Store_Original_Combo_Meals_v1.py
@@ -1,29 +1,18 @@
-"""Create dictionaries within nested dictionaries. These are the original combo
-menu, consisting of items with each item's price
+"""These are the original combo menu, consisting of items with each item's
+price (Using Dictionary Lists)
 """
 
-# Combo menu inside one large dictionary
-Combo_meals = {
-    "Value": {
-        "Beef Burger": 5.69,
-        "Fries": 1.00,
-        "Fizzy Drink": 1.00
-    },
-    "Cheezy": {
-        "Cheeseburger": 6.69,
-        "Fries": 1.00,
-        "Fizzy Drink": 1.00
-    },
-    "Super": {
-        "Cheeseburger": 6.69,
-        "Large Fries": 2.00,
-        "Smoothie": 2.00
-    }
-}
+# Combo menu inside one large list
+menu = [
+    ["Value", {"Beef Burger": 5.69, "Fries": 1.00, "Fizzy Drink": 1.00}],
+    ["Cheezy", {"Cheeseburger": 6.69, "Fries": 1.00, "Fizzy Drink": 1.00}],
+    ["Super", {"Cheeseburger": 6.69, "Large Fries": 2.00, "Smoothie": 2.00}]
+]
 
 # Print the Combo menu with format
-for combo_name, items_prices in Combo_meals.items():
-    print(f"\nCombo Name: {combo_name}\nItems & Prices:")
-
-    for key in items_prices:
-        print(f"{key}: {items_prices[key]}")
+for combo in menu:
+    combo_name = combo[0]
+    combo_items = combo[1]
+    print(f"Combo Name: {combo_name}")
+    for item, price in combo_items.items():
+        print(f"\t{item}: {price:.2f}")
