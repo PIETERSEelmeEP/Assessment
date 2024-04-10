b0e30420f5937af60a73ed585417802af958332a
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-10 20:01:42 +1200

    Create 03_Add_New_Combo_Meal_v2.py

---
+++ 03_Add_New_Combo_Meal_v2.py
@@ -0,0 +1,34 @@
+"""Improve 03_Add_New_Combo_Meal_v1 by using easygui
+"""
+
+Combo_meals = {
+    "Value": {
+        "Beef Burger": 5.69,
+        "Fries": 1.00,
+        "Fizzy Drink": 1.00
+    },
+    "Cheezy": {
+        "Cheeseburger": 6.69,
+        "Fries": 1.00,
+        "Fizzy Drink": 1.00
+    },
+    "Super": {
+        "Cheeseburger": 6.69,
+        "Large Fries": 2.00,
+        "Smoothie": 2.00
+    }
+}
+
+# Create a dictionary for the new combo
+combo_name = input("Enter the name of the new combo meal: ")
+combo_items = {}
+for _ in range(3):
+    item_name = input("Enter item name: ")
+    item_price = float(input("Enter item price: "))
+    combo_items[item_name] = item_price
+
+# Moving the new dictionary within the Combo meals dictionary
+Combo_meals[combo_name] = combo_items
+
+# printing the new list
+print(Combo_meals)
