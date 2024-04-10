b40442858a33e7a8f62167fd9db1e51eaebf9121
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-10 18:37:22 +1200

    Update Store_Original_Combo_Meals_v1.py

--- Store_Original_Combo_Meals_v1.py
+++ Store_Original_Combo_Meals_v1.py
@@ -2,6 +2,7 @@
 menu, consisting of items with each item's price
 """
 
+# Combo menu inside one large dictionary
 Combo_meals = {
     "Value": {
         "Beef Burger": 5.69,
@@ -20,6 +21,7 @@ Combo_meals = {
     }
 }
 
+# Print the Combo menu with format
 for combo_name, items_prices in Combo_meals.items():
     print(f"\nCombo Name: {combo_name}\nItems & Prices:")
 
