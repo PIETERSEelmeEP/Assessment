245b80e314df2e5de8091800ca2c55eeb9268c0e
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-10 18:00:31 +1200

    Update Information_Welcome_Message_v1.py

--- Information_Welcome_Message_v1.py
+++ Information_Welcome_Message_v1.py
@@ -4,7 +4,10 @@ buttonbox should appear and ask if they want to 'Add', 'Search', 'Delete',
 """
 import easygui
 
+# Welcome Message
 easygui.msgbox("Welcome", title="Welcome Message")
+
+# Select Action for Combo Menu Message & Buttonbox
 easygui.buttonbox("Please select action for the Combo Menu:",
                   title="Select Action", choices=["Add", "Search", "Delete",
                                                   "Print Full Menu",
