a3997558e150bfa76419751f9c8fb57567d978c9
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-10 19:31:14 +1200

    Create 02_Store_Original_Combo_Meals_v4.py

---
+++ 02_Store_Original_Combo_Meals_v4.py
@@ -0,0 +1,34 @@
+"""Turned the printing program into a function to allow reuse later on
+"""
+
+
+# Print the Combo menu with format function
+def print_combo_menu():
+    for combo_name, items_prices in Combo_meals.items():
+        print(f"\nCombo Name: {combo_name}\nItems & Prices:")
+
+        for key in items_prices:
+            print(f"{key}: {items_prices[key]}")
+
+
+# Combo menu inside one large dictionary
+Combo_meals = {
+    "Value": {
+        "Beef Burger": 5.69,
+        "Fries": 1.00,
+        "Fizzy Drink": 1.00
+    },
+    "Cheezy": {
+        "Cheeseburger": 6.69,
+        "Fries": 1.00,
+        "Fizzy Drink": 1.00
+    },
+    "Super": {
+        "Cheeseburger": 6.69,
+        "Large Fries": 2.00,
+        "Smoothie": 2.00
+    }
+}
+
+# Main Routine
+print_combo_menu()
