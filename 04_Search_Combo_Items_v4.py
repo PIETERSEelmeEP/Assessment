df96bd21d728dcb96f0db15b3e356a29bbef1d3d
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-04-12 08:44:41 +1200

    Update 04_Search_Combo_Items_v4.py

--- 04_Search_Combo_Items_v4.py
+++ 04_Search_Combo_Items_v4.py
@@ -9,8 +9,33 @@ def print_search_results(combo, combo_items):
         results_msg += f"{item_}: ${price:.2f}\n"
     easygui.msgbox(results_msg, title="Search Results")
 
+    # if option == "Search for specific contact":
+    #     found_contacts = []
+    #     search_for = easygui.enterbox("Enter the first name or last name of "
+    #                                   "the contact you are searching for",
+    #                                   title="Contact Searching For")
+    #
+    #     for contact_id, contact_info in contacts.items():
+    #         if search_for.lower() == contact_info[
+    #             "First Name"].lower() or search_for.lower() == \
+    #                 contact_info["Last Name"].lower():
+    #             found_contacts.append((contact_id, contact_info))
+    #
+    #     if found_contacts:
+    #         found_it = "Found contact(s):\n\n"
+    #
+    #         for contact_id, contact_info in found_contacts:
+    #             found_it += f"Contact ID: {contact_id}\n"
+    #
+    #             for key, value in contact_info.items():
+    #                 found_it += f"{key}: {value}\n"
+    #             found_it += "\n"
+    #
+    #         easygui.msgbox(found_it, title="Search Results")
+
 
 def search_in_combo():
+    found_combo = []
     combo_search = easygui.enterbox("Enter the name of the combo you are "
                                     "searching for:", title="Searching")
     found = False
